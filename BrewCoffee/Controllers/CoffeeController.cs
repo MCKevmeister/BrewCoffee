84519b1749b33ed500d2cf5d6759a1e7165e9dbc
Mark <markcristison@gmail.com>
2022-07-24 10:22:00 +1200

    Add to pass test

---
+++ BrewCoffee/Controllers/CoffeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrewCoffee.Controllers;
+
+[ApiController]
+[Route("brew-coffee")]
+public class CoffeeController: ControllerBase
+{
+    public CoffeeController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+}
